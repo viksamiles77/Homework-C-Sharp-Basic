3c3f903edd2da8ca6b39c43a88f36187c39a8acb
viksamiles77 <mileskiviktor77@gmail.com>
2024-02-29 15:21:44 +0100

    Task 2 added

---
+++ Homework_2/Task_2/Task_2/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input two numbers and I will swap their values of the variables.");
+            Console.WriteLine("Input the first number");
+            int number1;
+            string input1 = Console.ReadLine();
+            bool success1 = int.TryParse(input1, out number1);
+
+            Console.WriteLine("Input the second number");
+            int number2;
+            string input2 = Console.ReadLine();
+            bool success2 = int.TryParse(input2, out number2);
+
+            if (!success1 || !success2)
+            {
+                Console.WriteLine("Invalid input somewhere, please write a number!");
+            }
+            else
+            {
+                Console.WriteLine("Numbers before swap:");
+                Console.WriteLine($"First Number: {number1}");
+                Console.WriteLine($"Second Number: {number2}");
+
+                int temoporaryNumber = number1;
+                number1 = number2;
+                number2 = temoporaryNumber;
+
+                Console.WriteLine("Numbers after swap:");
+                Console.WriteLine($"First Number: {number1}");
+                Console.WriteLine($"Second Number: {number2}");
+            }
+
+
+
+        }
+    }
+}
