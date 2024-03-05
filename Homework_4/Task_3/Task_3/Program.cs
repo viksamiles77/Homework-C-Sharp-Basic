07773d4efe5759e08c223197489ca6ec230151c0
viksamiles77 <mileskiviktor77@gmail.com>
2024-03-05 16:44:36 +0100

    Task 3 done

---
+++ Homework_4/Task_3/Task_3/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = new string[] { "C#", "C++", "SQL", "Java", "Python" };
+            
+            decimal[] decimals = new decimal[] { 1.276m, 5.67m, 3.95m, 2.11m, 1.88m };
+            
+            char[] chars = new char[] { 'q', 'w', 'e', 'r', 't' };
+            
+            bool[] bools = new bool[] { true, false, true, true, false };
+            
+            int[][] twoWholeNumbers = new int[][] {
+            new int[] { 11, 22 },
+            new int[] { 33, 44 },
+            new int[] { 55, 66 },
+            new int[] { 77, 88 },
+            new int[] { 99, 00 }
+            };
+
+            //for (int i = 0; i < twoWholeNumbers[1].Length; i++)
+            //{
+            //    Console.WriteLine(twoWholeNumbers[3][i]);
+            //}
+        }
+    }
+}
