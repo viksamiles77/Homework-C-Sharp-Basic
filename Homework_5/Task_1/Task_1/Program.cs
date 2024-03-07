4691293cad6279b5b933e9152b8716e7a2cc537d
viksamiles77 <mileskiviktor77@gmail.com>
2024-03-07 16:23:41 +0100

    Task 1 done

---
+++ Homework_5/Task_1/Task_1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string birthdayIput;
+            bool validInput = false;
+
+            while (!validInput)
+            {
+                Console.WriteLine("Enter your birthday date (DD MM YYYY)");
+                string birthdayInput = Console.ReadLine();
+
+                if (IsValidDateFormat(birthdayInput))
+                {
+                    validInput = true;
+                    string[] datePart = birthdayInput.Split(' ');
+                    int day = int.Parse(datePart[0]);
+                    int month = int.Parse(datePart[1]);
+                    int year = int.Parse(datePart[2]);
+
+                    DateTime birthdayDate = new DateTime(year, month, day);
+
+                    int age = AgeCalculator(birthdayDate);
+                    Console.WriteLine($"Your age is {age} years old");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid date input, please enter a valid date format (DD MM YYYY).");
+                }
+
+            }
+            
+
+        }
+
+        static bool IsValidDateFormat(string input)
+        {
+            string[] datePart = input.Split(' ');
+            if (datePart.Length != 3)
+            {
+                return false;
+            }
+
+            int day;
+            int month;
+            int year;
+
+            if (!int.TryParse(datePart[0], out day) || !int.TryParse(datePart[1], out month) || !int.TryParse(datePart[2], out year))
+            {
+                return false;
+            }
+
+            if (day < 1 || day > 31 || month < 1 || month > 12 || year < 1000 || year > 9999)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        static int AgeCalculator(DateTime birthday)
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - birthday.Year;
+
+            if (today.Month < birthday.Month || (today.Month == birthday.Month && today.Day < birthday.Day))
+            {
+                age--;
+            }
+            return age;
+        }
+    }
+}
