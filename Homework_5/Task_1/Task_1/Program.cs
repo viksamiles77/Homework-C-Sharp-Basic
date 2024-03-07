968ce5a67128887903285555106a0f73485a3f3e
viksamiles77 <mileskiviktor77@gmail.com>
2024-03-07 17:48:01 +0100

    Task 2 done

--- Homework_5/Task_1/Task_1/Program.cs
+++ Homework_5/Task_1/Task_1/Program.cs
@@ -34,7 +34,7 @@ namespace Task_1
                 }
 
             }
-            
+
 
         }
 
--- Homework_5/Task_2/Task_2/Program.cs
+++ Homework_5/Task_2/Task_2/Program.cs
@@ -4,7 +4,52 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Substrings();
         }
+
+        static void Substrings()
+        {
+
+            string str = "Hello from SEDC Codecademy 2024";
+            int number;
+
+            while (true)
+            {
+                Console.WriteLine("Please enter a number.");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out number))
+                {
+                    if (number <= 0)
+                    {
+                        Console.WriteLine("Negative numbers not allowed. Please enter a number larger than 0!");
+                        continue;
+                    }
+
+                    if (number > str.Length)
+                    {
+
+                        Console.WriteLine($"The number exceeds the length of the string. The largest number is I can take is {str.Length}.");
+                        continue;
+                    }
+
+                    break;
+                }
+                Console.WriteLine("Wrong input.");
+            }
+
+            //str.ToCharArray(0, number);
+
+            //foreach (char c in str.ToCharArray(0, number))
+            //{
+            //    Console.WriteLine(c);
+            //}
+
+            Console.WriteLine(str.ToCharArray(0, number));
+            Console.WriteLine(str.ToCharArray(0, number).Length);
+
+
+        }
+
     }
 }
