cb100278de306d75cb51c4053bb9b068f919d425
viksamiles77 <mileskiviktor77@gmail.com>
2024-03-19 15:36:39 +0100

    Task 1 done

---
+++ Homework_9/Task_1/Task_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Transactions;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> numbers = new Queue<int>();
+
+            bool addNumbers = false;
+
+            do
+            {
+                addNumbers = false;
+                while (true)
+                {
+                    Console.Write("Enter number: ");
+                    string input = Console.ReadLine();
+
+                    int number;
+                    if (int.TryParse(input, out number))
+                    {
+                        numbers.Enqueue(number);
+                        break;
+                    }
+                    Console.WriteLine("Invalid iput. Please enter a valid number.");
+                }
+                Console.WriteLine("Do you want to add another number? (yes/no)");
+                string answer = Console.ReadLine();
+                if (answer.ToLower() == "yes" || answer.ToLower() == "y")
+                {
+                    addNumbers = true;
+                }
+            } while (addNumbers);
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
